cc364ab69e65ac3114402596e2a6a09bb74b37f8
GUSTAVO DE ABREU BECKER <gustavo.becker4@alunos.sc.senac.br>
2025-04-04 19:34:22 -0300

    Commit 2 aula 04/04

---
+++ database_gean/exercicios-mysql/cad_clientes/2 - Insert de 10 registros distintos.sql
@@ -0,0 +1,12 @@
+insert into clientes (nome, email, idade, cidade, estado) values 
+("Gustavo Becker", "gustavobecker123@email.com", 26, "Itajaí", "SC"),
+("Marcela Alecrim", "marcela123@gmail.com", 33, "Tangará", "SC"),
+("Almir Becker", "almirbecker123@email.com", 65, "São Paulo", "SP"),
+("Julio Cesar", "juliocesar123@email.com", 28, "Belo Horizonte", "MG"),
+("Maria Antonia", "mariaantonia@email.com", 32, "Bom Despacho", "MG"),
+("João Figueiredo", "joaofigueiredo@yahoo.com.br", 56, "Santos", "SP"),
+("Ana Vargas", "anavargas123@email.com", 31, "São Paulo", "SP"),
+("Arlete Carmen", "arletecarmen123@gmail.com", 28, "Teófilo Otoni", "MG"),
+("João Carlos", "joaocarlos@email.com", 29, "São Paulo", "SP"),
+("Pedro Telesforo", "pedropedreiro123@gmail.com", 29, "Iomerê", "SC");
+
