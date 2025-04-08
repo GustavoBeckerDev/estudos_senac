b116eba744897e9440532170b76e91369d62cecc
GUSTAVO DE ABREU BECKER <gustavo.becker4@alunos.sc.senac.br>
2025-04-07 22:12:04 -0300

    .

--- sistemas_ricardo/php/switchcase/ex16.php
+++ sistemas_ricardo/php/switchcase/ex16.php
@@ -2,38 +2,9 @@
 
 // Peça o nome de um feriado e use switch-case para verificar se é nacional, estadual ou desconhecido.
 
-$feriado = strtolower(readline("Digite o nome de um feriado: "));
-$feriado = trim($feriado);
-$feriado = str_replace ("-", "", $feriado);
-
-if (ctype_alpha($feriado)){
-
-    switch($feriado) {
-        case "sextafeirasanta":
-            echo "Feriado nacional. \n";
-            break;
-        case "tiradentes":
-            echo "Feriado nacional. \n";
-            break;
-        case "diadotrabalhador":
-            echo "Feriado nacional. \n";
-            break;
-        case "corpuschristi":
-            echo "Feriado nacional. \n";
-            break;
-            
-        
-        
 
 
 
 
-
-    }
-
-}
-else {
-    echo "Entrada de dados incorreta. \n";
-}
 
 ?>
