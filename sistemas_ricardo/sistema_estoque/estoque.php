7452f2170536b986130f4e4af6112f36fed6c316
Gustavo de Abreu Becker <gustavobecker654@gmail.com>
2025-05-13 21:20:05 -0300

    adicionando sistema de estoque

---
+++ sistemas_ricardo/sistema_estoque/estoque.php
@@ -0,0 +1,87 @@
+<?php
+
+$estoque = [];
+
+function menu() {
+    echo "\n===== MENU ESTOQUE =====\n";
+    echo "1. Listar produtos\n";
+    echo "2. Adicionar produto\n";
+    echo "3. Alterar produto\n";
+    echo "4. Remover produto\n";
+    echo "0. Sair\n";
+    echo "Escolha uma opção: ";
+}
+
+function listar($estoque) {
+    if (empty($estoque)) {
+        echo "Estoque vazio.\n";
+        echo "------------------------\n";
+        return;
+    }
+
+    echo "\n--- PRODUTOS NO ESTOQUE ---\n";
+    foreach ($estoque as $id => $produto) {
+        echo "ID: $id | Nome: {$produto['nome']} | Qtd: {$produto['quantidade']} | Valor: R$ {$produto['valor']}\n";
+    }
+}
+
+function adicionar(&$estoque) {
+    $nome = readline("Nome do produto: ");
+    $quantidade = (int) readline("Quantidade: ");
+    $valor = (float) readline("Valor (ex: 10.50): ");
+    $estoque[] = ['nome' => $nome, 'quantidade' => $quantidade, 'valor' => $valor];
+    echo "Produto adicionado com sucesso!\n";
+}
+
+function alterar(&$estoque) {
+    listar($estoque);
+    $id = (int) readline("Informe o ID do produto que deseja alterar: ");
+    if (!isset($estoque[$id])) {
+        echo "Produto não encontrado.\n";
+        return;
+    }
+
+    $estoque[$id]['nome'] = readline("Novo nome: ");
+    $estoque[$id]['quantidade'] = (int) readline("Nova quantidade: ");
+    $estoque[$id]['valor'] = (float) readline("Novo valor: ");
+    echo "Produto alterado com sucesso!\n";
+}
+
+function remover(&$estoque) {
+    listar($estoque);
+    $id = (int) readline("Informe o ID do produto que deseja remover: ");
+    if (!isset($estoque[$id])) {
+        echo "Produto não encontrado.\n";
+        return;
+    }
+
+    unset($estoque[$id]);
+    echo "Produto removido com sucesso!\n";
+}
+
+do {
+    menu();
+    $opcao = (int) readline();
+
+    switch ($opcao) {
+        case 1:
+            listar($estoque);
+            break;
+        case 2:
+            adicionar($estoque);
+            break;
+        case 3:
+            alterar($estoque);
+            break;
+        case 4:
+            remover($estoque);
+            break;
+        case 0:
+            echo "Saindo...\n";
+            break;
+        default:
+            echo "Opção inválida!\n";
+    }
+} while ($opcao !== 0);
+
+?>
