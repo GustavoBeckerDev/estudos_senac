667a6ed6a21899eaa75ad545e1542e06948a5f48
GUSTAVO DE ABREU BECKER <gustavo.becker4@alunos.sc.senac.br>
2025-08-12 22:05:47 -0300

    desenvolvimento web

---
+++ sistemas_ricardo/desenvolvedor-web/01/layout/header.php
@@ -0,0 +1,38 @@
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>FORMULÁRIO</title>
+  </head>
+
+  <body>
+
+    <main class="header">
+      <h2>FORMULÁRIO !!!</h2>
+    </main>
+
+    <style>
+
+        body {
+            font-family: "Roboto", sans-serif;
+        }
+
+        .header {
+            height: 80px;
+            width: 100%;
+            background: linear-gradient(to right, #1fa2ff, #12d8fa, #a6ffcb);
+            display: grid;
+            place-items: center;
+        }
+
+        .header > h2 {
+            font-family: "Roboto", sans-serif;
+            font-size: 1.5rem;
+            font-weight: 400;
+        }
+    </style>
